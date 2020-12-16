17398671b2eb82619d65ea5bb1e504d1500362f8
Fuzes Marcell <fuzes.marcell@gmail.com>
2020-12-16 10:32:53 +0100

    Finished day 14

--- code/day_13/day_13.go
+++ code/day_13/day_13.go
@@ -24,6 +24,57 @@ func FindBusInTRange(bus_a_time_stamp int64, bus_b_id int64, t_range int64) (boo
 	return false, 0
 }
 
+func FindCofForBusPair(bus_a_id int64, bus_b_id int64, t_range int64) (int64, int64) {
+	k := int64(1)
+	for true {
+		bus_a_time_stamp := bus_a_id * k
+		found, time_stamp := FindBusInTRange(bus_a_time_stamp, bus_b_id, t_range)
+		if found {
+			return k, time_stamp / bus_b_id
+		}
+
+		k += 1
+	}
+
+	return 0, 0
+}
+
+func TestIfChainOfBusesIsValid(bus_times []int64, next_time_stamp int64) bool {
+
+	sequence_found := false
+	prev_i := 0
+	for i := 1; i < len(bus_times); i++ {
+		bus_id := bus_times[i]
+		if bus_id != 0 {
+			found, time_stamp := FindBusInTRange(next_time_stamp, bus_id, int64(i - prev_i))
+			
+			if !found {
+				sequence_found = false
+				break
+			}
+
+			prev_i = i
+			next_time_stamp = time_stamp
+		}
+
+		sequence_found = true
+	}
+
+	return sequence_found
+}
+
+func GetFirstBusPairWithRange(bus_times []int64) (int64, int64, int64) {
+
+	prev_i := 0
+	for i := 1; i < len(bus_times); i++ {
+		if bus_times[i] != 0 {
+			return bus_times[0], bus_times[i], int64(i - prev_i)
+		}
+}
+
+	return 0, 0, 0
+}
+
 func main() {
 	our_departure_time := 1002461
 	var bus_departure_times = []int {29, 41, 521, 23, 13, 17, 601, 37, 19}
@@ -51,54 +102,25 @@ func main() {
 	fmt.Println("Part 01:", part_01_solution)
 
 	// var bus_times = []int64 {7,13,0,0,59,0,31,19}
-	var bus_times = []int64 {17,0,13,19}
+	// var bus_times = []int64 {17,0,13,19}
 	// var bus_times = []int64 {67,7,59,61}
 	// var bus_times = []int64 {67,0,7,59,61}
 	// var bus_times = []int64 {67,7,0,59,61}
 	// var bus_times = []int64 {1789,37,47,1889}
-	// var bus_times = []int64 {29,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,41,0,0,0,0,0,0,0,0,0,521,0,0,0,0,0,0,0,23,0,0,0,0,13,0,0,0,17,0,0,0,0,0,0,0,0,0,0,0,0,0,601,0,0,0,0,0,37,0,0,0,0,0,0,0,0,0,0,0,0,19}
+	var bus_times = []int64 {29,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,41,0,0,0,0,0,0,0,0,0,521,0,0,0,0,0,0,0,23,0,0,0,0,13,0,0,0,17,0,0,0,0,0,0,0,0,0,0,0,0,0,601,0,0,0,0,0,37,0,0,0,0,0,0,0,0,0,0,0,0,19}
 	// var bus_times = []int64 {29,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,41,0,0,0,0,0,0,0,0,0,521}
 
 
-	k := int64(1)
-	fmt.Println("Crunching away...")
-	// k := int64(100000000000000 / bus_times[0])
-	final_time_stamp := int64(0)
-	sequence_found := false
-	longest_sequence := 0
-	for !sequence_found {
-
-		next_time_stamp := bus_times[0] * k
-		prev_i := 0
-		for i := 1; i < len(bus_times); i++ {
-			bus_id := bus_times[i]
-			if bus_id != 0 {
-				found, time_stamp := FindBusInTRange(next_time_stamp, bus_id, int64(i - prev_i))
-				
-				if !found {
-					sequence_found = false
-					break
-				} else {
-					if bus_id == 13 {
-						fmt.Println(bus_times[0], k, bus_id, time_stamp / bus_id)
-					}
-					if i > longest_sequence {
-						longest_sequence = i
-						progress := float32(longest_sequence) / (float32(len(bus_times)) * 0.01)
-						fmt.Printf("%f%% [%d, %d %d]\n", progress, longest_sequence, bus_id, k)
-					}
-				}
-
-				prev_i = i
-				next_time_stamp = time_stamp
-			}
-
-			sequence_found = true
-		}
-
-		final_time_stamp = bus_times[0] * k
-		k += 1
+	a_id, b_id, t_range := GetFirstBusPairWithRange(bus_times)
+	fmt.Println(a_id, b_id, t_range)
+	a_cof, b_cof := FindCofForBusPair(a_id, b_id, t_range)
+	fmt.Println(a_cof, b_cof)
+	// time_stamp := a_id * a_cof
+	time_stamp := int64(100000000000436)
+	for !TestIfChainOfBusesIsValid(bus_times, time_stamp) {
+		a_cof += b_id
+		time_stamp = a_id * a_cof
 	}
 
-	fmt.Println("Part 02:", final_time_stamp, 100000000000000 - k)
+	fmt.Println(time_stamp)
 }
